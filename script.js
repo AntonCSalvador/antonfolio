88e7cfea7fc57d7b364b9f854f44b9f6fc8b7691
AntonCSalvador <antoncsalvador@Gmail.com>
2023-09-18 17:17:20 -0400

    pushed improved github links for airbnb

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu(){
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+document.getElementById('nightModeToggle').addEventListener('click', function() {
+    document.body.classList.toggle('night-mode');
+    
+    // Save the current mode to local storage
+    if (document.body.classList.contains('night-mode')) {
+        localStorage.setItem('night-mode', 'enabled');
+    } else {
+        localStorage.setItem('night-mode', 'disabled');
+    }
+});
+
+// Check local storage for theme preference
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (localStorage.getItem('night-mode') === 'enabled') {
+        document.body.classList.add('night-mode');
+    } else {
+        document.body.classList.remove('night-mode');
+    }
+});
