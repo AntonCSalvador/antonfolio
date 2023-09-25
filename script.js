30493213cce950bcb97102132f7994841d68af0f
AntonCSalvador <antoncsalvador@Gmail.com>
2023-09-25 09:33:04 -0400

    made the icons look better

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ function toggleMenu(){
 }
 
 document.getElementById('nightModeToggle').addEventListener('click', function() {
+    var moonIcon = document.getElementById('moonIcon');
+    var sunIcon = document.getElementById('sunIcon');
+
+    // Check which icon is currently displayed and toggle accordingly
+    if(getComputedStyle(moonIcon).display !== 'none') {
+        moonIcon.style.display = 'none';
+        sunIcon.style.display = 'block';
+        // Additional logic for enabling night mode
+    } else {
+        moonIcon.style.display = 'block';
+        sunIcon.style.display = 'none';
+        // Additional logic for disabling night mode
+    }
+
     document.body.classList.toggle('night-mode');
     
     // Save the current mode to local storage
